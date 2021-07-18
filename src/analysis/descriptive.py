14bb118a87df254bc3caa4e432a68afe0953a2b4
John James <john.james@nov8.ai>
2021-07-18 13:18:13 -0400

    archive

---
+++ src/analysis/descriptive.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#==============================================================================#
+# Project  : Drug Approval Analytics                                           #
+# Version  : 0.1.0                                                             #
+# File     : \src\analysis\descriptive.py                                      #
+# Language : Python 3.9.5                                                      #
+# -----------------------------------------------------------------------------#
+# Author   : John James                                                        #
+# Company  : nov8.ai                                                           #
+# Email    : john.james@nov8.ai                                                #
+# URL      : https://github.com/john-james-sf/drug-approval-analytics          #
+# -----------------------------------------------------------------------------#
+# Created  : Sunday, July 18th 2021, 8:32:43 am                                #
+# Modified : Sunday, July 18th 2021, 1:18:12 pm                                #
+# Modifier : John James (john.james@nov8.ai)                                   #
+# -----------------------------------------------------------------------------#
+# License  : BSD 3-clause "New" or "Revised" License                           #
+# Copyright: (c) 2021 nov8.ai                                                  #
+#==============================================================================#
+"""Classes that support descriptive analytics."""
+import os
+import pandas as pd
+# -----------------------------------------------------------------------------#
