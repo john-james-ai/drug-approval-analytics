f4b41273a07c28d7be2e02ac0b924976730b3fcf
John James <john.james@nov8.ai>
2021-08-13 06:35:23 -0400

    simplified user profiles

--- tests/test_platform/conftest.py
+++ tests/test_platform/conftest.py
@@ -12,64 +12,140 @@
 # URL      : https://github.com/john-james-sf/drug-approval-analytics         #
 # --------------------------------------------------------------------------  #
 # Created  : Sunday, August 8th 2021, 8:37:47 am                              #
-# Modified : Thursday, August 12th 2021, 2:50:25 am                           #
+# Modified : Friday, August 13th 2021, 6:35:22 am                             #
 # Modifier : John James (john.james@nov8.ai)                                  #
 # --------------------------------------------------------------------------- #
 # License  : BSD 3-clause "New" or "Revised" License                          #
 # Copyright: (c) 2021 nov8.ai                                                 #
 # =========================================================================== #
+import logging
+
 import pytest
 import pandas as pd
 
-from src.platform.database.admin import DBAdmin, TableAdmin, UserAdmin
-from src.platform.config import dba_credentials, test_credentials
-from src.platform.config import test_pg_credentials
-# --------------------------------------------------------------------------  #
+from src.platform.database.connect import PGConnectionFactory
+from src.platform.database.connect import SAConnectionFactory
+from src.platform.database.admin import DBAdmin, UserAdmin
+from src.platform.config import dba_credentials, test_pg_credentials
+from src.platform.config import test_credentials
+# --------------------------------------------------------------------------- #
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="class")
-def build_test_database():
+def dba_connection():
     """Builds a test database and returns a connection."""
-    admin = DBAdmin(dba_credentials)
-    admin.delete(name="test")
-    admin.create(name="test")
+    PGConnectionFactory.initialize(dba_credentials)
+    connection = PGConnectionFactory.get_connection()
+    connection.set_session(autocommit=True)
+    return connection
 
 
 @pytest.fixture(scope="class")
-def build_test_table():
-    # Drop the table if it exists.
-    admin = TableAdmin(dba_credentials)
-    admin.delete(name="source")
-
-    # Get table data
-    filepath = "./data/metadata/datasources.xlsx"
-    df = pd.read_excel(filepath, index_col=0)
-
-    # Load the data
-    admin.load(name='source', data=df)
-    admin.close()
-
-
-@pytest.fixture(scope="class")
-def drop_test_database():
+def test_pg_connection():
     """Builds a test database and returns a connection."""
-    admin = DBAdmin(dba_credentials)
-    admin.drop(name="test")
-    admin.close()
+    PGConnectionFactory.initialize(test_pg_credentials)
+    connection = PGConnectionFactory.get_connection()
+    connection.set_session(autocommit=True)
+    return connection
 
 
 @pytest.fixture(scope="class")
-def tear_down():
+def test_connection():
+    """Builds a test database and returns a connection."""
+    PGConnectionFactory.initialize(test_credentials)
+    connection = PGConnectionFactory.get_connection()
+    connection.set_session(autocommit=True)
+    return connection
+
+
+@pytest.fixture(scope="class")
+def sa_connection():
+    """Builds a test database and returns a connection."""
+    SAConnectionFactory.initialize(test_credentials)
+    return SAConnectionFactory.get_connection()
+
+
+@pytest.fixture(scope="class")
+def tear_down(dba_connection):
     """Returns the database to pretest state."""
-    dbadmin = DBAdmin(dba_credentials)
-    if dbadmin.exists("test"):
-        dbadmin.delete("test")
-    dbadmin.commit()
-    dbadmin.close()
+    connection = dba_connection
+    useradmin = UserAdmin()
+    if useradmin.exists(name=test_credentials.user,
+                        connection=connection):
+        useradmin.revoke(name=test_credentials.user,
+                         dbname=test_credentials.dbname,
+                         connection=connection)
 
-    useradmin = UserAdmin(dba_credentials)
-    if useradmin.exists(test_credentials.user):
-        useradmin.revoke(test_credentials.user, test_pg_credentials.dbname)
-        useradmin.delete(test_credentials.user)
-    useradmin.commit()
-    useradmin.close()
+    if useradmin.exists(name=test_pg_credentials.user,
+                        connection=connection):
+        useradmin.revoke(name=test_pg_credentials.user,
+                         dbname=test_credentials.dbname,
+                         connection=connection)
+        useradmin.revoke(name=test_pg_credentials.user,
+                         dbname=dba_credentials.dbname,
+                         connection=connection)
+
+    dbadmin = DBAdmin()
+    if dbadmin.exists("test", connection):
+        dbadmin.terminate_database_processes("test", connection)
+        dbadmin.delete("test", connection)
+
+    useradmin.delete(name=test_credentials.user,
+                     connection=connection)
+    useradmin.delete(name=test_pg_credentials.user,
+                     connection=connection)
+
+
+@pytest.fixture(scope="class")
+def onboard_user(dba_connection, test_pg_connection, test_connection):
+    # Create test user
+    logger.debug("\n\n\t\t\tOnboarding User\n")
+    connection = dba_connection
+    admin = UserAdmin()
+    admin.create(test_pg_credentials.user,
+                 test_pg_credentials.password, connection)
+    logger.debug("\n\n\t\t\tCreated test_pg\n")
+    # Grant privileges on postgres
+    useradmin = UserAdmin()
+    useradmin.grant(test_pg_credentials.user,
+                    dba_credentials.dbname, connection)
+    logger.debug("\n\n\t\t\tGranted privileges to test_pg\n")
+
+    # User creates test database on own credentials
+    connection = test_pg_connection
+    dbadmin = DBAdmin()
+    dbadmin.create("test", connection)
+    logger.debug("\n\n\t\t\tTest created test database\n")
+
+    # User creates connection to new database
+    logger.debug("\n\n\t\t\tReturning the connection\n")
+    connection = test_pg_connection
+
+    return connection
+
+
+@pytest.fixture(scope="class")
+def tear_down_test_metadata_database(dba_connection, test_pg_connection,
+                                     test_connection):
+    """Returns the database to pretest state."""
+    connection = test_pg_connection
+    logger.debug("\n\n\t\t\tTear Down Test Metadata Database\n")
+    dbadmin = DBAdmin()
+    dbadmin.delete("test", connection)
+    connection.commit()
+    PGConnectionFactory.return_connection(connection)
+    logger.debug(
+        "\n\n\t\t\tTear Down Test Metadata Database: Deleted test database\n")
+
+    connection = dba_connection
+    useradmin = UserAdmin()
+    useradmin.revoke_user(test_pg_credentials.user,
+                          test_pg_credentials.dbname, connection)
+    logger.debug(
+        "\n\n\t\t\tTear Down Test Metadata Database: Revoked privileges\n")
+    useradmin.delete_user(test_credentials.user, connection)
+    connection.commit()
+    PGConnectionFactory.return_connection(connection)
+    logger.debug(
+        "\n\n\t\t\tTear Down Test Metadata Database: Deleted user and committed\n")
